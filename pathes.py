e66cc69e930bf19cae0fefa3de9d786a8fb49edb
Bism4rk_x(laptop) <bism4rk228@gmail.com>
2025-09-29 18:31:09 +0500

    first commit

---
+++ pathes.py
@@ -0,0 +1,4 @@
+items_count = "//*[@id='root']/div/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div/div/div[1]/header/div[3]/div/div[1]/div/div/a/span"
+main_block = "//*[@id='root']/div/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div/div/div[2]/div[2]/div[1]/div/div/div/div[2]/div"
+title = "//*[@id='root']/div/div/div[1]/div[1]/div[2]/div[2]/div/div/div/div/div[2]/div[2]/div/div[1]/div/div/div/div/div[1]/h1/span[1]"
+        
